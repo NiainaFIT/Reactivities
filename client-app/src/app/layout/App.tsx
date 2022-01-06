de61fc05bdf5affa6169a17c12585bbd1749adcc
Niaina Sadiković-Ratsimanohatra <niaina.s.r@edu.fit.ba>
2022-01-06 13:25:54 +0100

    Section 10 Error handling (api and client)

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,10 +4,13 @@ import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import { observer } from 'mobx-react-lite';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
-
+import TestError from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
 
 
 function App() {
@@ -15,7 +18,7 @@ function App() {
   const location = useLocation();
   return (
     <>
-
+    <ToastContainer position='bottom-right' hideProgressBar/>
  <Route exact path='/' component={HomePage} />
     <Route
      path={'/(.+)'}/**any route that matches .plus something*/
@@ -23,10 +26,15 @@ function App() {
         <> 
       <NavBar/>
       <Container style = {{ marginTop:'7em' }}>
-        <Route exact path = '/' component = { HomePage }/>
-        <Route exact path = '/activities' component = { ActivityDashboard }/>
-        <Route path = '/activities/:id' component = { ActivityDetails }/>
-        <Route key={ location.key } path = { ['/createActivity', '/manage/:id'] } component = { ActivityForm }/>
+        <Switch>
+          <Route exact path = '/' component = { HomePage }/>
+          <Route exact path = '/activities' component = { ActivityDashboard }/>
+          <Route path = '/activities/:id' component = { ActivityDetails }/>
+          <Route key={ location.key } path = { ['/createActivity', '/manage/:id'] } component = { ActivityForm }/>
+          <Route path = '/errors' component={ TestError }/>
+          <Route path='/server-error' component = {ServerError}/>
+          <Route component={NotFound}/>
+        </Switch>
       </Container> 
         </>
       )}
--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,13 +1,16 @@
 import { createContext, useContext } from "react";
 import ActivityStore from "./activityStore";
+import CommonStore from "./commonStore";
 
 interface Store{
-    activityStore: ActivityStore /**classes can be used as types*/
+    activityStore: ActivityStore; /**classes can be used as types*/
+    commonStore: CommonStore;
 }
 
 export const store: Store = {/**object store of type Store*/
 
-    activityStore: new ActivityStore() /**property activityStore instantiated as ActivityStore*/
+    activityStore: new ActivityStore(), /**property activityStore instantiated as ActivityStore*/
+    commonStore: new CommonStore()
 }
 
 /**so store would be available in react context*/
